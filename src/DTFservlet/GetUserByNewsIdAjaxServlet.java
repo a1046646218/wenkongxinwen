a04c706700a2050d2705bf75bf28d189c4d3c89d
a1046646218 <38356038+a1046646218@users.noreply.github.com>
2019-05-23 13:29:16 +0800

    额，，上一个传少了

---
+++ src/DTFservlet/GetUserByNewsIdAjaxServlet.java
@@ -0,0 +1,35 @@
+package DTFservlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSON;
+
+import neu.edu.dao.impl.FollowingDaoImpl;
+import neu.edu.entity.User;
+import neu.edu.service.ForUserService;
+
+/**
+ * Servlet implementation class GetUserByNewsIdAjaxServlet
+ */
+@WebServlet("/GetUserByNewsIdAjaxServlet")
+public class GetUserByNewsIdAjaxServlet extends HttpServlet {
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int new_id = Integer.parseInt(request.getParameter("new_id"));
+		User u = (User) request.getSession().getAttribute("user");
+		User newspublish = new ForUserService().getUserBynewId(new_id);
+		String jsonString = JSON.toJSONString(newspublish);
+		System.out.println(jsonString);
+		response.getWriter().println(jsonString);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
